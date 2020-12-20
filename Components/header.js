6c370b459efec5db7252b53ed8b5254d7b54b163
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2020-12-19 20:24:42 -0500

    Add react-burger-menu.

--- Components/header.js
+++ Components/header.js
@@ -1,32 +1,35 @@
 import React from "react"
 import Link from "next/link"
 import useIsMobile from "./useIsMobile";
+import { slide as Menu } from 'react-burger-menu';
 
 const Header = () => {
   const isMobile = useIsMobile();
 
-  if (!isMobile) {
-    return (
-      <section className="header">
-        <div className="header-name">
-          <Link href="/">ZW</Link>
-        </div>
+
+  return (
+    <section className="header">
+      <div className="header-name">
+        <Link href="/">ZW</Link>
+      </div>
+      { !isMobile ?
         <div className="header-nav">
           <Link href="/">About Me</Link>
           <Link href="/experience">Experience</Link>
           <Link href="/projects">Projects</Link>
           <a className="resume" href="https://github.com/zefwang/resume/raw/master/zefeng_wang_resume.pdf">Resume</a>
+        </div> :
+        <div className="header-nav-mobile">
+          <Menu right >
+            <Link href="/" className="menu-time">About Me</Link>
+            <Link href="/experience" className="menu-time">Experience</Link>
+            <Link href="/projects" className="menu-time">Projects</Link>
+            <a className="resume" href="https://github.com/zefwang/resume/raw/master/zefeng_wang_resume.pdf">Resume</a>
+          </Menu>
         </div>
-      </section>
-    )
-  } else {
-    return (
-      <section className='header-mobile'>
-        Mobile
-      </section>
-    )
-  }
-
+      }
+    </section>
+  )
 }
 
 export default Header
