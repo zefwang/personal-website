d306dc22e3733044ebf0e8efe840db075adb7984
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2020-12-20 22:38:21 -0500

    fix resume link.

--- pages/header.js
+++ pages/header.js
@@ -11,7 +11,7 @@ const Header = () => {
         <Link href="/">About Me</Link>
         <Link href="/experience">Experience</Link>
         <Link href="/projects">Projects</Link>
-        <a className="resume" href="https://github.com/zefwang/resume/raw/master/zefeng_wang_resume.pdf">Resume</a>
+        <a className="resume" href="https://zefeng-wang.com/resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a>
       </div>
     </section>
   )
